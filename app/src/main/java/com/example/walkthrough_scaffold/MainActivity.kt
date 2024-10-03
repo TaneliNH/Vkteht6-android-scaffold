6bc92fd402e75164884c87d68759756ee7f08e7a
Taneli Heikkilä <heikkilataneli@gmail.com>
2024-10-03 23:29:44 +0300

    Korjailuja, nyt toimii paremmin.

--- app/src/main/java/com/example/walkthrough_scaffold/MainActivity.kt
+++ app/src/main/java/com/example/walkthrough_scaffold/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -62,7 +64,7 @@ fun ScaffoldApp() {
     }
 }
 
-@OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopBar(title: String, navController: NavController) {
     var expanded = remember { mutableStateOf(false) }
@@ -79,28 +81,30 @@ fun MainTopBar(title: String, navController: NavController) {
             DropdownMenu(
                 expanded = expanded.value,
                 onDismissRequest = { expanded.value = false }) {
-                DropdownMenuItem(onClick = { navController.navigate("info")}) {
-                    Text(text = "Info")
-                }
-                DropdownMenuItem(onClick = { navController.navigate("settings")}) {
-                    Text(text = "Settings")
-                }
+                DropdownMenuItem(
+                    text = { Text("Info") },
+                    onClick = { navController.navigate("info")}
+                )
+                DropdownMenuItem(
+                    text = { Text("Settings")},
+                    onClick = { navController.navigate("settings")}
+                )
             }
         }
     )
 }
 
-@OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenTopBar(title: String, navController: NavController) {
-    TopAppBar(
-        title = { Text(title) },
-        navigationIcon = {
-            IconButton(onClick = {navController.navigateUp() }) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+        TopAppBar(
+            title = { Text(title) },
+            navigationIcon = {
+                IconButton(onClick = {navController.navigateUp() }) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+                }
             }
-        }
-    )
+        )
 }
 
 @Composable
